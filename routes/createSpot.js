11739fc0f07629ade6091dcfd89789092f13d817
Mariana Duarte <guilhermespedro@outlook.com>
2019-09-18 12:52:21 +0100

    createSpot routes

--- routes/createSpot.js
+++ routes/createSpot.js
@@ -46,9 +46,11 @@ router.get("/createSpots/:spotsId/editspots", (req, res, next) => {
 router.post("/createSpots/:spotsId/editspots", (req, res, next) => {
   let spotsId = req.params.spotsId
   createSpots.findByIdAndUpdate(spotsId, {
-    name: req.body.name,
-    occupation: req.body.occupation,
-    catchPhrase: req.body.catchPhrase
+    name: req.body.name, 
+    adress: req.body.adress,
+    lat: req.body.lat,
+    lng: req.body.lng,
+    description: req.body.description
   }).then(() => {
     res.redirect("/editspots/"+ spotsId);
   });
